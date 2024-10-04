ceb8f8abd5e994fd16a13033b052448e3dc103d1
SabbathBraboza <157228790+SabbathBraboza@users.noreply.github.com>
2024-10-04 12:11:09 +0530

    Menu UI Is Done Working

---
+++ Assets/0_Main/Scripts/UI/UI Animation/Restaurant_App_Aniamtion.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Restaurant_App_Aniamtion : MonoBehaviour
+{
+    [Header("Lean Tween Settings:")]
+    [SerializeField] private LeanTweenType Type;
+
+
+    [Header("RectTransform:")]
+    [SerializeField] private RectTransform MenuPanel;
+
+
+
+}
