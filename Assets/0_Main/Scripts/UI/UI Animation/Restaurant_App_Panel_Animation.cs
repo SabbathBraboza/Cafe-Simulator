c6efd53108d94b9c90d376525810cd24864152ab
SabbathBraboza <157228790+SabbathBraboza@users.noreply.github.com>
2024-10-29 12:12:48 +0530

    Worked on Account Section

--- Assets/0_Main/Scripts/UI/UI Animation/Restaurant_App_Panel_Animation.cs
+++ Assets/0_Main/Scripts/UI/UI Animation/Restaurant_App_Panel_Animation.cs
@@ -37,64 +37,71 @@ public class Restaurant_App_Panel_Animation : MonoBehaviour
     [Header("Inventory Content Animation:")]
     [SerializeField] private RectTransform InventoryPanel;
 
+    [Space(10f)]
+    [Header("Account Content Animation:")]
+    [SerializeField] private RectTransform AccountPanel;
+
+    [Space(10f)]
+    [Header("Main Content Animation")]
     [SerializeField] private RectTransform MainContentPanel;
 
     public void MainContentAnimation(bool value) => MainContentPanel.LeanMoveLocalX(value ? -660 : 0, 0.2f).setEase(Type);
-    
+
     #region Cooking Animation Panel 
     public void OpenCookingContent(bool value)
     {
-       CookingContent.LeanMoveLocalX(value ? 0 : 680, 0.2f).setEase(Type);
+        CookingContent.LeanMoveLocalX(value ? 0 : 680, 0.2f).setEase(Type);
     }
     public void MenuPanelOpen(bool value)
-      {
-            if(value)
-            {
-                  MenuPanel.position = MenuPosition.position;
-                  MenuPanel.LeanScale(value ? Vector3.one : Vector3.zero,0.2f).setEase(Type);
-                  MenuPanel.LeanMove(value ? Vector3.one : Vector3.zero, 0.2f).setEase(Type);
-            }
-            else  
-            {
-                  MenuPanel.LeanScale(value ? Vector2.one : Vector2.zero, 0.2f).setEase(Type);
-                  MenuPanel.LeanMove(value ? Vector3.one : Vector3.zero, 0.2f).setEase(Type).setDelay(0.02f);
-            }
-      }
-     public void RecipePanelOpen(bool value) 
-      { 
-             if(value)  
-             {
-                  RecipePanel.position = RecipePosition.position;
-                  RecipePanel.LeanScale(value ? Vector3.one : Vector3.zero, 0.1f).setEase(Type);
-                  RecipePanel.LeanMove(value ? Vector3.one : Vector3.zero, 0.1f).setEase(Type);
-             }
-             else  
-             {
-                  RecipePanel.LeanScale(value ? Vector2.one : Vector2.zero, 0.2f).setEase(Type);
-                  RecipePanel.LeanMove(value ? Vector3.one : Vector3.zero, 0.2f).setEase(Type).setDelay(0.02f);
-             }
-      }
-      public void CookingPanelOpen(bool value) 
-      { 
-        if(value)
-        {
-                  CookingPanel.position = CookingPosition.position;
-                  CookingPanel.LeanScale(value ? Vector3.one : Vector3.zero, 0.2f).setEase(Type);
-                  CookingPanel.LeanMove(value ? Vector3.one : Vector3.zero, 0.2f).setEase(Type);
-        }
-      else{
-                  CookingPanel.LeanScale(value ? Vector2.one : Vector2.zero, 0.2f).setEase(Type);
-                  CookingPanel.LeanMove(value ? Vector3.one : Vector3.zero, 0.2f).setEase(Type).setDelay(0.02f);
-            }
-      }
-    public void FoodPanelAnimation(bool value) 
     {
-        if (value) 
+        if (value)
         {
-          FoodPanel.position = FoodPosition.position;
+            MenuPanel.position = MenuPosition.position;
+            MenuPanel.LeanScale(value ? Vector3.one : Vector3.zero, 0.2f).setEase(Type);
+            MenuPanel.LeanMove(value ? Vector3.one : Vector3.zero, 0.2f).setEase(Type);
+        }
+        else
+        {
+            MenuPanel.LeanScale(value ? Vector2.one : Vector2.zero, 0.2f).setEase(Type);
+            MenuPanel.LeanMove(value ? Vector3.one : Vector3.zero, 0.2f).setEase(Type).setDelay(0.02f);
+        }
+    }
+    public void RecipePanelOpen(bool value)
+    {
+        if (value)
+        {
+            RecipePanel.position = RecipePosition.position;
+            RecipePanel.LeanScale(value ? Vector3.one : Vector3.zero, 0.1f).setEase(Type);
+            RecipePanel.LeanMove(value ? Vector3.one : Vector3.zero, 0.1f).setEase(Type);
+        }
+        else
+        {
+            RecipePanel.LeanScale(value ? Vector2.one : Vector2.zero, 0.2f).setEase(Type);
+            RecipePanel.LeanMove(value ? Vector3.one : Vector3.zero, 0.2f).setEase(Type).setDelay(0.02f);
+        }
+    }
+    public void CookingPanelOpen(bool value)
+    {
+        if (value)
+        {
+            CookingPanel.position = CookingPosition.position;
+            CookingPanel.LeanScale(value ? Vector3.one : Vector3.zero, 0.2f).setEase(Type);
+            CookingPanel.LeanMove(value ? Vector3.one : Vector3.zero, 0.2f).setEase(Type);
+        }
+        else
+        {
+            CookingPanel.LeanScale(value ? Vector2.one : Vector2.zero, 0.2f).setEase(Type);
+            CookingPanel.LeanMove(value ? Vector3.one : Vector3.zero, 0.2f).setEase(Type).setDelay(0.02f);
+        }
+    }
+    public void FoodPanelAnimation(bool value)
+    {
+        if (value)
+        {
+            FoodPanel.position = FoodPosition.position;
             FoodPanel.LeanScale(value ? Vector3.one : Vector3.zero, 0.2f).setEase(Type);
             FoodPanel.LeanMove(value ? Vector3.one : Vector3.zero, 0.2f).setEase(Type);
-        } 
+        }
         else
         {
             FoodPanel.LeanScale(value ? Vector2.one : Vector2.zero, 0.2f).setEase(Type);
@@ -124,19 +131,27 @@ public class Restaurant_App_Panel_Animation : MonoBehaviour
     }
     public void OpenCartAnimation(bool value)
     {
-        if (value) { 
-         CartPanel.position = CartButtonPosition.position;
+        if (value)
+        {
+            CartPanel.position = CartButtonPosition.position;
             CartPanel.LeanMove(value ? Vector3.one : Vector3.zero, 0.3f).setEase(Type);
             CartPanel.LeanScale(value ? Vector3.one : Vector3.zero, 0.3f).setEase(Type);
         }
-        else {
+        else
+        {
             CartPanel.LeanMove(value ? Vector3.one : Vector3.zero, 0.4f).setEase(Type);
-            CartPanel.LeanScale(value ? Vector2.one : Vector2.zero, 0.4f).setEase(Type); 
+            CartPanel.LeanScale(value ? Vector2.one : Vector2.zero, 0.4f).setEase(Type);
         }
     }
     #endregion
 
     #region Inventory Animation Panel 
     public void OpenInventoryPanel(bool value) => InventoryPanel.LeanMoveLocalX(value ? 0 : 680, 0.2f).setEase(Type);
+    #endregion
+
+    #region Account Animation Panel
+
+    public void OpenAccountPanel(bool value) => AccountPanel.LeanMoveLocalX(value ? 0 : 680, 0.2f).setEase(Type);
+
     #endregion
 }
